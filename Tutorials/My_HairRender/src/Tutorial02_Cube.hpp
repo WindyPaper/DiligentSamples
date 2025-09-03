d7cad4a7a5d62c3c0351ee4de90146559127b1ca
WindyPaper <windy_max@163.com>
2025-09-03 15:09:45 +0800

    Update.

--- Tutorials/My_HairRender/src/Tutorial02_Cube.hpp
+++ Tutorials/My_HairRender/src/Tutorial02_Cube.hpp
@@ -33,6 +33,10 @@
 
 #include "HairRender.h"
 
+extern "C" { __declspec(dllexport) extern const unsigned int D3D12SDKVersion = 616; }
+
+extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = u8".\\D3D12\\"; }
+
 namespace Diligent
 {
 
